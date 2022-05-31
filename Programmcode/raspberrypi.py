bfb01e9fc0d5dac5da7cadd6f69ae8521724fc2c
PascalRombach <pasc.rombach@heinrichheinedo.de>
2022-05-31 14:53:36 +0200

    Added basic communication between Pi and cam

--- Programmcode/raspberrypi.py
+++ Programmcode/raspberrypi.py
@@ -1,1 +1,18 @@
-import buildhat
+from serial_com import Communicator
+
+com = Communicator("COM5")
+
+def main():
+    try:
+        while True:
+            print(*com.wait_for_data(),sep="\n")
+            pass
+        pass
+    finally:
+        com.close()
+        pass
+    pass
+
+if __name__ == "__main__":
+    main()
+    pass
