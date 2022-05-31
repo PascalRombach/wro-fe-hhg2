86f63bad3ec8cbde03a1859a97f921c45e851092
PascalRombach <pasc.rombach@heinrichheinedo.de>
2022-05-31 16:03:18 +0200

    Actually added basic concept for RPi script

--- Programmcode/raspberrypi.py
+++ Programmcode/raspberrypi.py
@@ -1,11 +1,36 @@
-from serial_com import Communicator
+from serial_com import Communicator, Blob
+from serial.tools import list_ports_linux
+import buildhat
+import time
+import RPi.GPIO as GPIO
 
-com = Communicator("COM5")
+def frame(steering_motor, driving_motor, distance, im_size, blobs : list[Blob]):
+    
+    pass
 
 def main():
+    print(*list_ports_linux.comports(),sep="\n")
+    cam_port = input("Enter the name for the comport: ")
+
+    GPIO_Port = 5
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(GPIO_Port, GPIO.IN)
+
+    com = Communicator(cam_port)
+    steering_motor = buildhat.Motor("B")
+    driving_motor = buildhat.Motor("A")
+    distance_sensor = buildhat.DistanceSensor("C")
+
+    while not GPIO.input(GPIO_Port): 
+        time.sleep(1)
+        pass
+
+    driving_motor.start(100)
     try:
         while True:
-            print(*com.wait_for_data(),sep="\n")
+            im_size, blobs = com.wait_for_data()
+
+            frame(steering_motor, driving_motor, distance_sensor.get_distance(), im_size,blobs)
             pass
         pass
     finally:
