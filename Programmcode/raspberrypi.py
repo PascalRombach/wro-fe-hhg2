f51a9d83929a631b66736c661976d181a73be4a0
albrechtf0 <101705366+albrechtf0@users.noreply.github.com>
2022-06-09 08:35:31 +0200

    Improving algo stability

--- Programmcode/raspberrypi.py
+++ Programmcode/raspberrypi.py
@@ -6,7 +6,7 @@ import buildhat
 import time
 import RPi.GPIO as GPIO
 
-driveline = 0#set correct data
+driveline = 10#set correct data
 #hardwhare info setup distance sensor to the left
 
 def makeTurn(right: bool, steering):#make the car turn aproxamatly 90 degrees will correct error automaticly probably 
@@ -30,24 +30,27 @@ def frame(steering, drive, distance, distance2, imageSize, blobs: list[Blob]):
         return
     
     
-    redSice = 0
-    greenSice = 0
+    redSize = 0
+    greenSize = 0
     treshold = 0#set Treshold for minimal sice
     for blob in blobs: 
         if blob.type == "red_pillar":
-            redSice += blob.size
+            redSize += blob.size
         elif blob.type == "green_pillar":
-            greenSice += blob.size
+            greenSize += blob.size
     
-    if redSice > treshold and redSice>greenSice:
+    if redSize > treshold and redSize>greenSize:
         driveline = 0 #set correct data
-    elif greenSice > treshold and greenSice > redSice:
+    elif greenSize > treshold and greenSize > redSize:
         driveline = 0#set correct data
-    
-    if distance2 > driveline:
-        steering.run_to_position(100, blocking=False) #add correct data to go left 
-    elif distance2 < driveline:
-        steering.run_to_position(-100, blocking=False) #add correct data to go right 
+        
+    if distance2 <= 1000: # correct sidebarier distance catch
+        if distance2 > driveline+20: # correct exeptet error range 
+            steering.run_to_position(10, blocking=False) #add correct data to go left 
+        elif distance2 < driveline-20:#correct exepted error 
+            steering.run_to_position(-10, blocking=False) #add correct data to go right 
+        else:
+            steering.run_to_position(o, blocking=False)#make vehicel go streight 
 
 def main():
     print(*list_ports_linux.comports(),sep="\n")
